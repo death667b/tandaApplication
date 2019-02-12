da04d2a89e9b5324d6b26e0f16ee1c7a289ae606
death667b <idaniel@spinnys.com>
2019-02-13 09:52:30 +1000

    Add Home Component

---
+++ client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Adnat</h1>
+          <p>A simple roster app</p>
+        </div>
+      </div>
+    );
+  }
+}
