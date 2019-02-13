a2fdffe527410fcda013cb79d2e17c28a6cfe1e8
death667b <idaniel@spinnys.com>
2019-02-13 11:30:06 +1000

    Enable cors

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const usersRouter = require("./router/users");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use("/auth", authRouter);
 app.use("/organisations", organisationsRouter);
 app.use("/shifts", shiftsRouter);
