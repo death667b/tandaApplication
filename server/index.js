b52d1e33e5c28efcdff94fb7a578eeac95fef9da
death667b <idaniel@spinnys.com>
2019-02-13 12:56:28 +1000

    sets CORS headers again

--- server/index.js
+++ server/index.js
@@ -9,8 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, UPDATE, PUT, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 app.use("/auth", authRouter);
